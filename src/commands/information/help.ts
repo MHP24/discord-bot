44d11f67e0b5dff2a91db4956d11b0ddc95e71cf
MHP24 <mig.henriquezp@gmail.com>
2023-08-25 15:55:08 -0400

    update: add help command description

--- src/commands/information/help.ts
+++ src/commands/information/help.ts
@@ -6,19 +6,24 @@ export const command = {
   data: new SlashCommandBuilder().setName('help')
     .setDescription('List all commands'),
   run: async (interaction: CommandInteraction) => {
-    const commands = getCommands().map(({ category, commands }) => {
-      return {
-        name: `**${category.replace(/^\w/gi, (w: string) => w.toUpperCase())}**`,
-        value: commands.join('\n').replace(/(.ts|.js)/gi, '')
-      };
-    });
+    try {
+      const commands = getCommands().map(({ category, commands }) => {
+        return {
+          name: `**${category.replace(/^\w/gi, (w: string) => w.toUpperCase())}**`,
+          value: commands.map(c => `\`${c}\``).join('\n').replace(/(.ts|.js)/gi, '')
+        };
+      });
 
-    const embed = new EmbedBuilder()
-      .setTitle('DMiguelo available commands')
-      .setThumbnail(bot.profilePicture)
-      .setColor(0x0099FF)
-      .setFields(commands)
-      .setFooter({ text: 'All commands support /', iconURL: bot.profilePicture });
-    interaction.reply({ embeds: [embed] });
+      const embed = new EmbedBuilder()
+        .setTitle('DMiguelo available commands')
+        .setThumbnail(bot.profilePicture)
+        .setColor(0x0099FF)
+        .setDescription('Don Miguelo is a cat who loves Coca-Cola\n written in typescript by `migueloooo`')
+        .setFields(commands)
+        .setFooter({ text: 'All commands must be used with /', iconURL: bot.profilePicture });
+      interaction.reply({ embeds: [embed] });
+    } catch (error) {
+      console.error({ error });
+    }
   }
 };
