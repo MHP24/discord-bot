b003f3a15db224aa063a49078dfb516fe20acac7
MHP24 <mig.henriquezp@gmail.com>
2023-08-26 16:09:12 -0400

    update: ping

--- src/commands/information/ping.ts
+++ src/commands/information/ping.ts
@@ -3,6 +3,13 @@ import { CommandInteraction, SlashCommandBuilder } from 'discord.js';
 export const command = {
   data: new SlashCommandBuilder().setName('ping').setDescription('Replies with Pong!'),
   run: async (interaction: CommandInteraction) => {
-    await interaction.reply('Pong!');
+    try {
+      await interaction.deferReply();
+      const reply = await interaction.fetchReply();
+      const ping = reply.createdTimestamp - interaction.createdTimestamp;
+      await interaction.editReply(`Pong! :ping_pong: Client ping ${ping}ms`);
+    } catch (error) {
+      console.error({ error });
+    }
   }
 };
