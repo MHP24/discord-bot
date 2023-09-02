d4b90cd4d7eff03bbe9285937b014f2423de0951
MHP24 <mig.henriquezp@gmail.com>
2023-09-02 02:09:24 -0400

    feat: add basic /pause and /resume command implementation

--- src/commands/music/resume.ts
+++ src/commands/music/resume.ts
@@ -2,7 +2,7 @@ import { CommandInteraction, GuildMember, SlashCommandBuilder } from 'discord.js
 import { songQueue } from '../../controllers';
 
 export const command = {
-  data: new SlashCommandBuilder().setName('skip').setDescription('Skip current song'),
+  data: new SlashCommandBuilder().setName('resume').setDescription('Resume current song'),
   run: async (interaction: CommandInteraction) => {
     try {
       const voiceChannel = (interaction.member as GuildMember).voice.channel;
@@ -13,10 +13,9 @@ export const command = {
       }
 
       const queue = songQueue.getQueue(interaction.guildId);
-      queue?.audioPlayer.stop();
       queue?.audioPlayer.unpause();
 
-      return await interaction.reply('Skipped');
+      return await interaction.reply('The music is back');
     } catch (error) {
       console.error({ error });
       return await interaction.reply('Internal error, try again');
